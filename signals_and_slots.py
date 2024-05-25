fcdc11cd319a279b470fe8a78eddedc31d17057c
elleinad <janjaaan12@gmail.com>
2024-05-25 23:11:10 +0800

    Version 1 of the code

---
+++ signals_and_slots.py
@@ -0,0 +1,16 @@
+#Version 1: Just responding to the button click : syntax
+from PySide6.QtWidgets import QApplication, QPushButton
+
+#The slot: responds when something happens
+def button_clicked():
+    print("Pinindot mo nga naman talaga!")
+
+app = QApplication()
+button = QPushButton("Sige! Pindutin mo!")
+
+#clicked is a signal of QPushButton. It's emitted when you click on the button
+#You can wire a slot to the signal using the syntax below:
+button.clicked.connect(button_clicked)
+
+button.show()
+app.exec()
