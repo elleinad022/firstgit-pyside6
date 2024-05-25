65a78e43fc1d6131b12fe3f0b022c8962082f9af
elleinad <janjaaan12@gmail.com>
2024-05-25 23:22:32 +0800

    Version 2 of signals and slots code

--- signals_and_slots.py
+++ signals_and_slots.py
@@ -1,4 +1,5 @@
 #Version 1: Just responding to the button click : syntax
+"""
 from PySide6.QtWidgets import QApplication, QPushButton
 
 #The slot: responds when something happens
@@ -10,6 +11,25 @@ button = QPushButton("Sige! Pindutin mo!")
 
 #clicked is a signal of QPushButton. It's emitted when you click on the button
 #You can wire a slot to the signal using the syntax below:
+button.clicked.connect(button_clicked)
+
+button.show()
+app.exec()
+"""
+
+#Version 2: Signals sending Values, Capture Values in Slots
+from PySide6.QtWidgets import QApplication, QPushButton
+
+#The Slot: responds when something happens
+def button_clicked(data):
+    print("Pinindot mo pa talagang inamo ka! Eto : ", data)
+
+app = QApplication()
+button = QPushButton("Wag mo pindotin.")
+button.setCheckable(True) #Makes the button checkable, its unchecked by default. Clicking = toggling
+
+#clicked is a signal of QPushButton. It's emitted when you click on the button
+#You can wire a slot to the signal using the syntax below
 button.clicked.connect(button_clicked)
 
 button.show()
