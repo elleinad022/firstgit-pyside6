7e142713fbdda23087bc0baa3c355761256e9882
elleinad <janjaaan12@gmail.com>
2024-05-26 18:07:29 +0800

    Added button layout in order to have the button laid on the main window/widget

--- qwidget/rockwidget.py
+++ qwidget/rockwidget.py
@@ -1,7 +1,12 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 class RockWidget (QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("RockWidget")
+        button1 = QPushButton("Button1")
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(button1)
+
+        self.setLayout(button_layout)
