7f8c232891fb67565ce63ba720a22a094533b937
elleinad <janjaaan12@gmail.com>
2024-05-26 18:11:26 +0800

    Trying vertical box layout

--- qwidget/rockwidget.py
+++ qwidget/rockwidget.py
@@ -1,12 +1,14 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
 
 class RockWidget (QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("RockWidget")
         button1 = QPushButton("Button1")
+        button2 = QPushButton("Button2")
 
-        button_layout = QHBoxLayout()
+        button_layout = QVBoxLayout()
         button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
 
         self.setLayout(button_layout)
