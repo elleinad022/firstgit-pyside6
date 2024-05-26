b27e7f6f8fa0fddee6770312540cd72ac4d31a0e
elleinad <janjaaan12@gmail.com>
2024-05-26 18:16:57 +0800

    Added signals and slots for button1 and button2

--- qwidget/rockwidget.py
+++ qwidget/rockwidget.py
@@ -4,11 +4,21 @@ class RockWidget (QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("RockWidget")
+        
         button1 = QPushButton("Button1")
+        button1.clicked.connect(self.button1_clicked)
+
         button2 = QPushButton("Button2")
+        button2.clicked.connect(self.button2_clicked)
 
         button_layout = QVBoxLayout()
         button_layout.addWidget(button1)
         button_layout.addWidget(button2)
 
         self.setLayout(button_layout)
+
+    def button1_clicked(self):
+        print("Button1 pinindot mo")
+
+    def button2_clicked(self):
+        print("Button2 pinindot mo")
