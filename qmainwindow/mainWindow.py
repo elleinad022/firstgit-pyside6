567658dd61ecb8b64b0a9a7b8856bc4eb308b4e9
elleinad <janjaaan12@gmail.com>
2024-05-26 18:59:59 +0800

    Added more menus and actions but only as placeholders and trials no assigned signals and slots

--- qmainwindow/mainWindow.py
+++ qmainwindow/mainWindow.py
@@ -9,11 +9,22 @@ class MainWindow(QMainWindow):
 
         #Menubar and menus
         menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("&File")
+        file_menu = menu_bar.addMenu("File")
         quit_action = file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit_app)
 
+        edit_menu = menu_bar.addMenu("Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
 
+        #More menus for trying
+        menu_bar.addMenu("Window")
+        menu_bar.addMenu("Setting")
+        menu_bar.addMenu("Help")
+        
 
     def quit_app(self):
         self.app.quit()
